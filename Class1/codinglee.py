e82ae8dc78739bf3e9b3272b0ba0b50d41f0d6df
owlish123 <15223089454@163.com>
2020-01-01 13:41:20 +0800

    This code is for the first Python class

---
+++ Class1/codinglee.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 30 22:22:26 2019
+
+@author: codinglee
+"""
+#import turtle #import turtle module
+#turtle.pensize(3) # Set pen thickness to 3 pixels
+#turtle.penup() # Pull the pen up
+#turtle.goto(-200, -50)
+#turtle.pendown() # Pull the pen down
+#turtle.begin_fill() # Begin to fill color in a shape
+#turtle.color("red")
+#turtle.circle(40, steps = 3) # Draw a triangle
+#turtle.end_fill() # Fill the shape
+#
+##import a new turtle module
+#import turtle
+#turtle.penup()
+#turtle.goto(0, -50)
+#turtle.pendown()
+#turtle.begin_fill() # Begin to fill color in a shape
+#turtle.color("green")
+#turtle.circle(40, steps = 5) # Draw a pentagon
+#turtle.end_fill() # Fill the shape
+#
+##import another turtle module
+#import turtle
+#turtle.penup()
+#turtle.goto(100, -50)
+#turtle.pendown()
+#turtle.begin_fill() # Begin to fill color in a shape
+#turtle.color("yellow")
+#turtle.circle(40, steps = 6) # Draw a hexagon
+#turtle.end_fill() # Fill the shape
+#
+##a new project
+#print 'Welcome to \nPython \nworld'
